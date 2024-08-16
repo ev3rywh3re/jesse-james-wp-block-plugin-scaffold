6f936e3eafcf6c2d7513628c4a6f10c69ed386d6
ev3rywh3re <jess@funroe.net>
2024-08-16 08:47:26 -0500

    Updated build files. Should work.

--- build/render.php
+++ build/render.php
@@ -10,14 +10,11 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
-// Get the current year.
-$current_year = date( "Y" );
 
 // Determine which content to display.
 if ( isset( $attributes['categoryId'] ) ) {
 
     $limit = -1; // Set to -1 to retrieve all images
-
     $featured_images = jess_get_post_ids_with_featured_image( $attributes['categoryId'], $limit );
 
 	$one_random = array_rand( $featured_images );
