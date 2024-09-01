f570be987ce45091f59e9fcc817ffae6ba3d66e0
ev3rywh3re <jess@funroe.net>
2024-08-31 23:58:35 -0500

    Get this to a working place before we do more tests!

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '8c757d772a15a8ecc7f7');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b49f5b4090c06f93bd68');
--- src/edit.js
+++ src/edit.js
@@ -1,45 +1,24 @@
 import { __ } from '@wordpress/i18n';
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
-import { PanelBody, SelectControl } from '@wordpress/components';
-import { useSelect } from '@wordpress/data';
+import { PanelBody, TextControl } from '@wordpress/components';
 
 export default function Edit({ attributes, setAttributes }) {
-    const { categoryId } = attributes;
+  const { categoryId } = attributes;
 
-    // Fetch categories using useSelect
-    const { categories, isLoading } = useSelect((select) => {
-        const { getCategories, isGetCategoriesLoading } = select('core');
-        return {
-            categories: getCategories({ per_page: -1 }), // Fetch all categories
-            isLoading: isGetCategoriesLoading(),
-        };
-    }, []);
-
-    return (
-        <>
-            <InspectorControls>
-                <PanelBody title={__('Settings', 'jess-block-scaffold-experiment')}>
-                    {isLoading ? (
-                        <p>Loading categories...</p>
-                    ) : (
-                        <SelectControl
-                            label={__('Category', 'jess-block-scaffold-experiment')}
-                            value={categoryId}
-                            options={[
-                                { value: '', label: 'Select a category' },
-                                ...categories.map((category) => ({
-                                    value: category.id,
-                                    label: category.name,
-                                })),
-                            ]}
-                            onChange={(value) => setAttributes({ categoryId: value })}
-                        />
-                    )}
-                </PanelBody>
-            </InspectorControls>
-            <div {...useBlockProps()}>
-                {/* ... rest of your block content ... */}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <InspectorControls>
+        <PanelBody title={__('Settings', 'jess-block-scaffold-experiment')}>
+          <TextControl
+            label={__('Category ID', 'jess-block-scaffold-experiment')}
+            value={categoryId}
+            onChange={(value) => setAttributes({ categoryId: parseInt(value, 10) })}
+          />
+        </PanelBody>
+      </InspectorControls>
+      <div {...useBlockProps()}>
+        {/* ... rest of your block content ... */}
+      </div>
+    </>
+  );
 }
